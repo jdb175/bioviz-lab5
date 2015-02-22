fe162636b98bbaf565e381d79a7f7629ff2370b8
Jason Whitehouse <jdb175@gmail.com>
2015-02-22 12:12:21 -0500

    Working DB connection

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'dev.codementum.org',
+  database : 'firefinder',
+  user     : 'wpi',
+  password : 'wpiwpi'
+});
+
+connection.connect();
+
+app.get('/points', function (req, res) {
+	//just using the give ranges
+	var minTemp = req.query.minTemp == undefined ? '2.2' : req.query.minTemp;
+	var maxTemp = req.query.maxTemp == undefined ? '33.0' : req.query.maxTemp;
+	var minHumidity = req.query.minHumidity == undefined ? '15' : req.query.minHumidity;
+	var maxHumidity = req.query.maxHumidity == undefined ? '100' : req.query.maxHumidity;
+	var minWind = req.query.minWind == undefined ? '0.4' : req.query.minWind;
+	var maxWind = req.query.maxWind == undefined ? '9.4' : req.query.maxWind;
+
+	var query = "select * from FORESTFIRE" + 
+	" where Temp between " + minTemp + " and " + maxTemp +
+	" and Humidity between " + minHumidity + " and " + maxHumidity + 
+	" and Wind between " + minWind + " and " + maxWind; // Example sql = "select * from " + table_name + " where Temp > " + minTemp;
+
+	console.log(query);
+
+	connection.query(query, function(err, rows, fields) {
+		if (err) throw err;
+		console.log(rows);
+		res.send(rows);
+	});
+})
+
+//connection.end();
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
